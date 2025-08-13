fc58855a06aa31dc2cb5b89976f45ad82f1f680a
santiago guarin <santiago.guarin@fracttal.com>
2025-08-13 08:49:55 -0500

    [TEST]

--- .__mf__temp/remote/localSharedImportMap.js
+++ .__mf__temp/remote/localSharedImportMap.js
@@ -3,71 +3,9 @@
 
     const importMap = {
       
-        "react": async () => {
-          let pkg = await import("__mf__virtual/remote__prebuild__react__prebuild__.js")
-          return pkg
-        }
-      ,
-        "react-dom": async () => {
-          let pkg = await import("__mf__virtual/remote__prebuild__react_mf_2_dom__prebuild__.js")
-          return pkg
-        }
-      
     }
       const usedShared = {
       
-          "react": {
-            name: "react",
-            version: "19.1.1",
-            scope: ["default"],
-            loaded: false,
-            from: "remote",
-            async get () {
-              usedShared["react"].loaded = true
-              const {"react": pkgDynamicImport} = importMap 
-              const res = await pkgDynamicImport()
-              const exportModule = {...res}
-              // All npm packages pre-built by vite will be converted to esm
-              Object.defineProperty(exportModule, "__esModule", {
-                value: true,
-                enumerable: false
-              })
-              return function () {
-                return exportModule
-              }
-            },
-            shareConfig: {
-              singleton: true,
-              requiredVersion: "^19.1.1"
-            }
-          }
-        ,
-          "react-dom": {
-            name: "react-dom",
-            version: "19.1.1",
-            scope: ["default"],
-            loaded: false,
-            from: "remote",
-            async get () {
-              usedShared["react-dom"].loaded = true
-              const {"react-dom": pkgDynamicImport} = importMap 
-              const res = await pkgDynamicImport()
-              const exportModule = {...res}
-              // All npm packages pre-built by vite will be converted to esm
-              Object.defineProperty(exportModule, "__esModule", {
-                value: true,
-                enumerable: false
-              })
-              return function () {
-                return exportModule
-              }
-            },
-            shareConfig: {
-              singleton: true,
-              requiredVersion: "^19.1.1"
-            }
-          }
-        
     }
       const usedRemotes = [
       ]
