3d3e01ee2f580dcf385262ab2506659c4d076f6b
santiago guarin <santiago.guarin@fracttal.com>
2025-08-13 08:32:13 -0500

    [TEST]

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,25 @@ import { federation } from '@module-federation/vite';
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { dependencies } from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),
   federation({
-    filename: 'remoteEntry.js',
-    name: 'remote',
-    library: { type: 'var', name: 'remote' }, // 👈 clave
-    exposes: {
-      './remote-app': './src/App.tsx',
-    },
-    remotes: {},
-    shared: {
-      react: { singleton: true, requiredVersion: undefined },
-      'react-dom': { singleton: true, requiredVersion: undefined },
-    }
-  }),
+				filename: 'remoteEntry.js',
+				name: 'remote',
+				exposes: {
+					'./remote-app': './src/App.tsx',
+				},
+				remotes: {},
+				shared: {
+					react: {
+						requiredVersion: dependencies.react,
+						singleton: true,
+					},
+				},
+			}),
   ],
   resolve: {
     alias: {
